b52e0d3851b71b36fc4fdc480af07026522ca742
Deeployed <techextremists24@gmail.com>
2025-05-11 21:59:23 +0300

    Update auth/utils.py

--- auth/utils.py
+++ auth/utils.py
@@ -3,7 +3,7 @@ import jwt
 SECRET_KEY = '1234567890'  # TODO: Move to environment variables
 
 def is_valid_password(password):
-    return len(password) > 8
+    return len(password) > 5
 
 def authenticate(u, p):
     if u == "demo@example.com" and p == "password123":
